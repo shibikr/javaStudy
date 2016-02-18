4a6746d8a339e37d1e2894811215ddce55a3737d
shibi <shibik@thoughtworks.com>
2016-02-18 18:14:32 +0530

    [shibi] - added function for addition of two matrices
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- javaAssignment/Matrix.java
+++ javaAssignment/Matrix.java
@@ -1,7 +1,7 @@
 public class Matrix{
 
-  public int row,column;
-  public int[][] matrix;
+  private int row,column;
+  private int[][] matrix;
 
   public Matrix(int row,int column){
     this.row = row;
@@ -9,23 +9,36 @@ public class Matrix{
     this.matrix = new int[row][column];
   };
 
-  public int[][] createMatrix(int[] values){
-    int count = 0;
-    for(int i = 0;i < this.row;i++){
-      for(int j = 0;j < this.column;j++){
-        matrix[i][j] = values[count];
-        count++;
+  public void initializeValues(int row,int column,int value){
+    this.matrix[row][column] = value;
+  };
+
+  public int getElementAt(int row,int column){
+    return this.matrix[row][column];
+  };
+
+  public boolean isOrderSame(Matrix matrix1){
+    return (this.row == matrix1.row) && (this.column == matrix1.column);
+  };
+
+  public Matrix addMatrix( Matrix matrix){
+    Matrix result = new Matrix(this.row,this.column);
+    if(this.isOrderSame(matrix)){
+      for(int i=0;i<this.row;i++){
+        for(int j=0;j<this.column;j++){
+            result.matrix[i][j] = this.getElementAt(i,j)+matrix.getElementAt(i,j);
+        };
       };
-    };
-    return matrix;
+      return result;
+    }
+    return null;
   };
 
   public void printMatrix(){
     for(int i = 0;i < this.row;i++){
       for(int j = 0;j < this.column;j++){
-        System.out.println(matrix[i][j]);
+        System.out.println(this.matrix[i][j]);
       };
     };
   };
-
 };
--- javaAssignment/MatrixTest.java
+++ javaAssignment/MatrixTest.java
@@ -3,14 +3,83 @@ import org.junit.Test;
 
 public class MatrixTest{
   @Test
-  public void createMatrixTest() {
-    Matrix m = new Matrix(2,2);
-    int[] values = {1,2,3,4};
-    int[][] matrix = m.createMatrix(values);
-    assertEquals(2,matrix[1].length);
-    assertEquals(1, matrix[0][0]);
-    assertEquals(2, matrix[0][1]);
-    assertEquals(3, matrix[1][0]);
-    assertEquals(4, matrix[1][1]);
+  public void initializeValues_and_getElementAt_Test() {
+    Matrix m = new Matrix(2,3);
+    int[] values = {1,2,3,4,5,6};
+    int count = 0;
+    for(int i = 0 ;i<2;i++){
+      for(int j=0;j<3;j++){
+        m.initializeValues(i,j,values[count]);
+        count++;
+      };
+    };
+    assertEquals(1, m.getElementAt(0,0));
+    assertEquals(2, m.getElementAt(0,1));
+    assertEquals(3, m.getElementAt(0,2));
+    assertEquals(4, m.getElementAt(1,0));
+    assertEquals(5, m.getElementAt(1,1));
+    assertEquals(6, m.getElementAt(1,2));
   };
+
+  @Test
+  public void isOrderSame_gives_true_if_both_matrices_have_same_order(){
+    Matrix m1 = new Matrix(2,2);
+    Matrix m2 = new Matrix(2,2);
+    boolean result = m1.isOrderSame(m2);
+    assertEquals(true,result);
+  };
+
+  @Test
+  public void isOrderSame_gives_false_if_both_matrices_dose_not_have_same_order(){
+    Matrix m1 = new Matrix(2,2);
+    Matrix m2 = new Matrix(2,3);
+    boolean result = m1.isOrderSame(m2);
+    assertEquals(false,result);
+  };
+
+  @Test
+  public void addMatrixTest_should_return_added_matrix_when_order_of_both_matrices_are_same(){
+    Matrix m1 = new Matrix(2,2);
+    Matrix m2 = new Matrix(2,2);
+    int[] values1 = {1,2,3,4};
+    int[] values2 = {1,2,3,4};
+    int count1 = 0,count2 = 0;
+    for(int i = 0 ;i<2;i++){
+      for(int j=0;j<2;j++){
+        m1.initializeValues(i,j,values1[count1]);
+        count1++;
+      };
+    };
+    for(int i = 0 ;i<2;i++){
+      for(int j=0;j<2;j++){
+        m2.initializeValues(i,j,values2[count2]);
+        count2++;
+      };
+    };
+    Matrix result = m1.addMatrix(m2);
+    assertEquals(2, result.getElementAt(0,0));
+  };
+
+  @Test
+  public void addMatrixTest_should_return_null_when_order_of_both_matrices_are_not_same(){
+    Matrix m1 = new Matrix(2,2);
+    Matrix m2 = new Matrix(2,3);
+    int[] values1 = {1,2,3,4};
+    int[] values2 = {1,2,3,4,5,6};
+    int count1 = 0,count2 = 0;
+    for(int i = 0 ;i<2;i++){
+      for(int j=0;j<2;j++){
+        m1.initializeValues(i,j,values1[count1]);
+        count1++;
+      };
+    };
+    for(int i = 0 ;i<2;i++){
+      for(int j=0;j<3;j++){
+        m2.initializeValues(i,j,values2[count2]);
+        count2++;
+      };
+    };
+    assertEquals(null, m1.addMatrix(m2));
+  }
+
 };
