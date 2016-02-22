72f20d20a5c753398cf35b32cadcfaa488489695
shibi <shibik@thoughtworks.com>
2016-02-22 09:47:00 +0530

    [shibi] -implemented function to add and multiply two  matrices
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- javaAssignment/Matrix.java
+++ javaAssignment/Matrix.java
@@ -17,16 +17,16 @@ public class Matrix{
     return this.matrix[row][column];
   };
 
-  public boolean isOrderSame(Matrix matrix1){
+  private boolean isOrderSame(Matrix matrix1){
     return (this.row == matrix1.row) && (this.column == matrix1.column);
   };
 
-  public Matrix addMatrix( Matrix matrix){
+  public Matrix addMatrix(Matrix matrix){
     Matrix result = new Matrix(this.row,this.column);
     if(this.isOrderSame(matrix)){
       for(int i=0;i<this.row;i++){
         for(int j=0;j<this.column;j++){
-            result.matrix[i][j] = this.getElementAt(i,j)+matrix.getElementAt(i,j);
+            result.initializeValues(i,j,this.getElementAt(i,j)+matrix.getElementAt(i,j));
         };
       };
       return result;
@@ -34,11 +34,42 @@ public class Matrix{
     return null;
   };
 
-  public void printMatrix(){
-    for(int i = 0;i < this.row;i++){
-      for(int j = 0;j < this.column;j++){
-        System.out.println(this.matrix[i][j]);
-      };
-    };
+  private boolean canBothMatrixMultiply(Matrix matrix){
+    return (this.column == matrix.row);
   };
+
+  public Matrix multiplyMatrix(Matrix matrix){
+    Matrix result = new Matrix(this.row,matrix.column);
+    int sum = 0;
+    if(this.canBothMatrixMultiply(matrix)){
+      for (int i = 0; i < this.row; i++) {
+        for (int j = 0; j < matrix.column; j++) {
+          for (int k = 0; k < matrix.row; k++) {
+            sum = sum + (this.getElementAt(i,k)*matrix.getElementAt(k,j));
+          }
+          result.initializeValues(i,j,sum);
+          sum = 0;
+        }
+      }
+      return result;
+    }
+    return null;
+  };
+
+  private boolean isSquareMatrix(){
+    return (this.row == this.column);
+  };
+
+  public int getDeterminant(){
+    int determinant = 0;
+    if(isSquareMatrix()){
+      if(this.row == 1)
+        return this.getElementAt(0,0);
+      if(this.row == 2)
+        return ((this.getElementAt(0,0)*this.getElementAt(1,1))
+               -(this.getElementAt(0,1)*this.getElementAt(1,0)));
+    }
+    return determinant;
+  };
+
 };
--- javaAssignment/MatrixTest.java
+++ javaAssignment/MatrixTest.java
@@ -22,22 +22,6 @@ public class MatrixTest{
   };
 
   @Test
-  public void isOrderSame_gives_true_if_both_matrices_have_same_order(){
-    Matrix m1 = new Matrix(2,2);
-    Matrix m2 = new Matrix(2,2);
-    boolean result = m1.isOrderSame(m2);
-    assertEquals(true,result);
-  };
-
-  @Test
-  public void isOrderSame_gives_false_if_both_matrices_dose_not_have_same_order(){
-    Matrix m1 = new Matrix(2,2);
-    Matrix m2 = new Matrix(2,3);
-    boolean result = m1.isOrderSame(m2);
-    assertEquals(false,result);
-  };
-
-  @Test
   public void addMatrixTest_should_return_added_matrix_when_order_of_both_matrices_are_same(){
     Matrix m1 = new Matrix(2,2);
     Matrix m2 = new Matrix(2,2);
@@ -80,6 +64,61 @@ public class MatrixTest{
       };
     };
     assertEquals(null, m1.addMatrix(m2));
-  }
+  };
+
+  @Test
+  public void multiplyMatrix_should_give_multiplied_matrix(){
+    Matrix m1 = new Matrix(3,3);
+    Matrix m2 = new Matrix(3,3);
+    int[] values = {1,2,3,4,5,6,7,8,9};
+    int count = 0;
+    for(int i = 0 ;i<3;i++){
+      for(int j=0;j<3;j++){
+        m1.initializeValues(i,j,values[count]);
+        m2.initializeValues(i,j,values[count]);
+        count++;
+      };
+    };
+    Matrix result = m1.multiplyMatrix(m2);
+    assertEquals(result.getElementAt(0,0),30);
+    assertEquals(result.getElementAt(0,1),36);
+    assertEquals(result.getElementAt(0,2),42);
+    assertEquals(result.getElementAt(1,0),66);
+    assertEquals(result.getElementAt(1,1),81);
+    assertEquals(result.getElementAt(1,2),96);
+    assertEquals(result.getElementAt(2,0),102);
+    assertEquals(result.getElementAt(2,1),126);
+    assertEquals(result.getElementAt(2,2),150);
+  };
+
+  @Test
+  public void multiplyMatrix_should_give_null_if_matrices_can_not_be_multiplied(){
+    Matrix m1 = new Matrix(3,2);
+    Matrix m2 = new Matrix(3,3);
+    assertEquals(null,m1.multiplyMatrix(m2));
+  };
+
+  @Test
+  public void getDeterminant_should_return_0_if_matrix_is_not_square_matrix(){
+    Matrix m = new Matrix(3,2);
+    assertEquals(0,m.getDeterminant());
+  };
+
+  @Test
+  public void getDeterminant_should_return_the_value_of_the_matrix_if_matrix_is_of_order_1x1(){
+    Matrix m = new Matrix(1,1);
+    m.initializeValues(0,0,5);
+    assertEquals(5,m.getDeterminant());
+  };
+
+  @Test
+  public void getDeterminant_should_return_the_determinant_of_a_2x2_matrix(){
+    Matrix m = new Matrix(2,2);
+    m.initializeValues(0,0,1);
+    m.initializeValues(0,1,2);
+    m.initializeValues(1,0,3);
+    m.initializeValues(1,1,4);
+    assertEquals(-2,m.getDeterminant());
+  };
 
 };
