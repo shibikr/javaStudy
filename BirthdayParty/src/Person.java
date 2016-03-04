06c219636f9fc9ce42e3300497ca19fb4b7bbfb1
shibi <shibik@thoughtworks.com>
2016-03-04 14:06:59 +0530

    added functions for 4 types format

--- BirthdayParty/src/Person.java
+++ BirthdayParty/src/Person.java
@@ -28,12 +28,12 @@ public class Person {
         return name.getLastFirst();
     }
 
-    private String getFirstNameFirstWithCountry(){
-        return (name.getFirstLast()+", "+address.country);
+    private String getFirstNameFirstWithCountry() {
+        return (name.getFirstLast() + ", " + address.country);
     }
 
-    private String getLastNameFirstWithCountry(){
-        return (name.getLastFirst()+", "+address.country);
+    private String getLastNameFirstWithCountry() {
+        return (name.getLastFirst() + ", " + address.country);
     }
 
     public String getSpecifiedFormat(String format) {
@@ -43,9 +43,9 @@ public class Person {
                 return getFirstNameFirst();
             case "lastFirst":
                 return getLastNameFirst();
-            case "firstLastCountry" :
+            case "firstLastCountry":
                 return getFirstNameFirstWithCountry();
-            case "lastFirstCountry" :
+            case "lastFirstCountry":
                 return getLastNameFirstWithCountry();
             default:
                 return getFirstNameFirst();
